ba46dd18ae93122450de19ff0de4a43868614aac
a-shashkin <caezar1993@gmail.com>
2022-04-22 15:56:53 +0400

    Committing build.gradle (parameters set) and DemoQATest.java with source code for tests

---
+++ src/test/java/com/simbirsoft/DemoQATest.java
@@ -0,0 +1,38 @@
+package com.simbirsoft;
+
+import org.junit.jupiter.api.*;
+
+public class DemoQATest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all tests\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before test\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("I love SimbirSoft\n");
+    }
+
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After test\n");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("I really love SimbirSoft\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all tests\n");
+    }
+
+}
